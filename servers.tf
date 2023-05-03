a8a2c00a8dc3de15f2f0adfb18274ee4d57a958f
Raghu K <raghuk.vit@gmail.com>
2023-05-03 07:22:49 +0530

    Update roboshop-terraform/

--- servers.tf
+++ servers.tf
@@ -7,6 +7,23 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/raghudevopsb72/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
 }
 
 
