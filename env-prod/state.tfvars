ea533e2f87dde42790e68cd985e793120d5d6161
Raghu K <raghuk.vit@gmail.com>
2023-05-03 06:47:30 +0530

    Terraform

---
+++ env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-a72"
+key    = "roboshop/prod/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-a72"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
