1d03c4111b0dc1f4ec72fb99040d80ad33c6252f
Raghu K <raghuk.vit@gmail.com>
2023-05-04 07:14:56 +0530

    Update roboshop-terraform/

--- module/main.tf
+++ module/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = var.component_name
+    Name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   }
 }
 
