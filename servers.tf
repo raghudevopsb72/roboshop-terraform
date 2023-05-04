509acc800d1a92e5100a6a1644f7ad68c0338adc
Raghu K <raghuk.vit@gmail.com>
2023-05-04 06:17:47 +0530

    Update roboshop-terraform/

--- servers.tf
+++ servers.tf
@@ -7,14 +7,19 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+}
 
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each   = var.components
   provisioner "remote-exec" {
 
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = self.private_ip
+      host     = aws_instance.instance[each.value["name"]].private_ip
     }
 
     inline = [
