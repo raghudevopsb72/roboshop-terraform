66c6669f8637ca1bc2e5bd66d21d3145f97b4a7e
Raghu K <raghuk.vit@gmail.com>
2023-05-11 06:43:04 +0530

    Update roboshop-terraform/

--- module/locals.tf
+++ module/locals.tf
@@ -7,8 +7,9 @@ locals {
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/raghudevopsb72/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    //    "sudo labauto ansible",
+    //    "ansible-pull -i localhost, -U https://github.com/raghudevopsb72/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "echo OK"
   ]
 }
 
