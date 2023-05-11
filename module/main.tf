d323223a1f9a5b0551675284767cdcd632ca46f8
Raghu K <raghuk.vit@gmail.com>
2023-05-11 06:44:58 +0530

    Update roboshop-terraform/

--- module/main.tf
+++ module/main.tf
@@ -54,10 +54,8 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_role_policy" "ssm-ps-policy" {
+resource "aws_iam_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.role.id
-
 
   policy = jsonencode({
     "Version" : "2012-10-17",
