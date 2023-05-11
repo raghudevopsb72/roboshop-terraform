05b9baecea69e16d963d348585c045506f7345e1
Raghu K <raghuk.vit@gmail.com>
2023-05-11 06:48:45 +0530

    Update roboshop-terraform/

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = local.name
   }
@@ -54,8 +54,15 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_policy" "ssm-ps-policy" {
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component_name}-${var.env}-role"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
 
   policy = jsonencode({
     "Version" : "2012-10-17",
