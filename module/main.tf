183e3bd1aa6ac2c8a17cecbc69e0db8bb7dea03a
Raghu K <raghuk.vit@gmail.com>
2023-05-12 06:10:26 +0530

    Update roboshop-terraform/

--- module/main.tf
+++ module/main.tf
@@ -71,18 +71,16 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Sid" : "VisualEditor0",
         "Effect" : "Allow",
         "Action" : [
+          "kms:Encrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component_name}.*"
-      },
-      {
-        "Sid" : "VisualEditor1",
-        "Effect" : "Allow",
-        "Action" : "ssm:DescribeParameters",
-        "Resource" : "*"
+        "Resource" : [
+          "arn:aws:kms:us-east-1:633788536644:key/dce90622-5a23-4f82-a639-be841f534702",
+          "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component_name}.*"
+        ]
       }
     ]
   })
