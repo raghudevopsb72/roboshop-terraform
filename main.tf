924265ec349dac579cd04e1f9bd09bd08891efd7
Raghu K <raghuk.vit@gmail.com>
2023-05-25 07:30:27 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ module "vpc" {
 }
 
 
-module "web" {
-  source = "git::https://github.com/raghudevopsb72/tf-module-app.git"
-
-  for_each      = var.app
-  instance_type = each.value["instance_type"]
-  subnet_id     = element(lookup(lookup(lookup(module.vpc, "main", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
-}
+//module "web" {
+//  source = "git::https://github.com/raghudevopsb72/tf-module-app.git"
+//
+//  for_each      = var.app
+//  instance_type = each.value["instance_type"]
+//  subnet_id     = element(lookup(lookup(lookup(module.vpc, "main", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+//}
 //element(lookup(lookup(module.vpc, each.value["subnet_name"], null), "subnet_ids", null), 0)
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "vpc" {
-  value = module.vpc
+  value = lookup(module.vpc, "main", null)
 }
