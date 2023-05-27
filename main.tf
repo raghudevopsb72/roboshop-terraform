5daf2576ed59e856f6dd0add7ee0cbf6f77c7350
Raghu K <raghuk.vit@gmail.com>
2023-05-27 06:10:28 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -13,24 +13,24 @@ module "vpc" {
 }
 
 
-module "app" {
-  source = "git::https://github.com/raghudevopsb72/tf-module-app.git"
-
-  for_each         = var.app
-  instance_type    = each.value["instance_type"]
-  name             = each.value["name"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size         = each.value["max_size"]
-  min_size         = each.value["min_size"]
-
-  env          = var.env
-  bastion_cidr = var.bastion_cidr
-  tags         = local.tags
-
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-}
+//module "app" {
+//  source = "git::https://github.com/raghudevopsb72/tf-module-app.git"
+//
+//  for_each         = var.app
+//  instance_type    = each.value["instance_type"]
+//  name             = each.value["name"]
+//  desired_capacity = each.value["desired_capacity"]
+//  max_size         = each.value["max_size"]
+//  min_size         = each.value["min_size"]
+//
+//  env          = var.env
+//  bastion_cidr = var.bastion_cidr
+//  tags         = local.tags
+//
+//  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+//  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+//  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+//}
 
 
 
