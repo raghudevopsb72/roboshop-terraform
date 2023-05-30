fa25c45f9ef371b8637b52ad1012389ee1be9551
Raghu K <raghuk.vit@gmail.com>
2023-05-30 06:41:25 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -109,16 +109,16 @@ module "rabbitmq" {
 module "alb" {
   source = "git::https://github.com/raghudevopsb72/tf-module-alb.git"
 
-  for_each      = var.alb
-  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_alb_cidr = each.value.["name"] == "public" ? [ "0.0.0.0/0" ] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
-  name = each.value.["name"]
-  internal = each.value["internal"]
+  for_each       = var.alb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_alb_cidr = each.value["name"] == "public" ? ["0.0.0.0/0"] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
+  name           = each.value["name"]
+  internal       = each.value["internal"]
 
 
-  tags         = local.tags
-  env          = var.env
-  vpc_id       = local.vpc_id
+  tags   = local.tags
+  env    = var.env
+  vpc_id = local.vpc_id
 }
 
 
